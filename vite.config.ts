0e3599b5494483e9224f832c1e940fe3600faeb2
bogatibiraj <birajbogati2@gmail.com>
2026-09-26 04:13:44 +0545

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
+  tanstackStart: {
+    prerender: {
+      enabled: true,
+      autoSubfolderIndex: true,
+      autoStaticPathsDiscovery: true,
+      crawlLinks: true,
+      failOnError: true,
+    },
+  },
+
   vite: {
     base: process.env.VITE_BASE_PATH || "/",
   },
